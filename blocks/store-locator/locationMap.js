0adf42670332665b674c8e613f5dfa8ff3bb02ee
Mark Szulc <mszulc@adobe.com>
2025-02-20 20:26:24 +1100

    Offer & Store Locator Blocks

---
+++ blocks/store-locator/locationMap.js
@@ -0,0 +1,64 @@
+import { h, render, Component } from '../../scripts/preact.js';
+import htm from '../../scripts/htm.js';
+const html = htm.bind(h);
+const GOOGLE_MAP_KEY='AIzaSyB4er8NcF-CGHY4ELZbqMlqzAkgsyt798g';
+
+class LocationMap extends Component {
+
+  componentDidMount() {
+    this.loadGoogleMapsApi().then(() => {
+      this.map = new google.maps.Map(this.mapContainer, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+        disableDefaultUI: true,
+        keyboardShortcuts: false,
+        styles: [
+          {
+            featureType: "all",
+            stylers: [
+              { saturation: -100 },
+              { visibility : "simplified" }
+            ]
+          }
+        ]
+      });
+    });
+  }
+
+  loadGoogleMapsApi() {
+    return new Promise((resolve) => {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_KEY}`;
+      script.async = true;
+      script.defer = true;
+      script.addEventListener('load', resolve);
+      document.body.appendChild(script);
+    });
+  }
+
+
+  handleSearchClick() {
+    alert('Hold up.. no searching just yet!');
+  }
+
+  render(props) {
+    return html`
+    <div className="shopfinder">
+        <div className="sidepanel">
+          <h1 className="sidepanel__title">${props.heading}</h1>
+          <div className="search">
+            <p className="search__title">Find another location</p>
+            <div className="search__box">
+               <input className="search__input" type="text" placeholder="Post Code" name="search"></input>
+               <button className="search__button" onClick=${this.handleSearchClick}>Search</button>
+            </div>
+          </div>
+        </div>
+        <div className="map" ref=${el => this.mapContainer = el}>
+        </div>
+    </div>
+    `;
+  }
+}
+
+export default LocationMap;
---
+++ blocks/store-locator/store-locator.js
@@ -0,0 +1,57 @@
+export async function initMap() {
+// eslint-disable-next-line no-undef
+const { Map } = await google.maps.importLibrary('maps');
+
+const map = new Map(document.getElementById('locator-map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    disableDefaultUI: true,
+    keyboardShortcuts: false,
+    styles: [
+      {
+        featureType: "all",
+        stylers: [
+          { lightness : -5 },
+          { saturation: -100 },
+          { visibility : "simplified" }
+        ]
+      }
+    ]  });
+  // eslint-disable-next-line no-undef
+  const infoWindow = new google.maps.InfoWindow({
+    map,
+  });
+
+}
+
+
+
+export default function decorate(block) {
+
+  const pText = block.querySelector('p').textContent;
+  block.textContent = '';
+  
+  window.initMap = async () => {
+    initMap();
+  };
+
+  const locatorDOM = document.createRange().createContextualFragment(`
+  <div class="shopfinder">
+    <div class="sidepanel">
+      <h3 class="sidepanel__title">Try a new roast at a Fréscopa near you!</h3>
+    <div class="search">
+      <p class="search__title">Find another location</p>
+      <div class="search__box">
+        <input class="search__input" type="text" placeholder="Post Code" name="search"></input>
+        <button class="search__button">Search</button>
+      </div>
+    </div>
+    </div>
+      <div class="map" id="locator-map">
+    </div>
+  </div>
+  `)
+
+  block.append(locatorDOM);
+
+}
