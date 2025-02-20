5912db1ebc51c1e3e31a5796cc52844790a5cf1f
Mark Szulc <mszulc@adobe.com>
2025-02-20 20:44:34 +1100

    aem environment config

---
+++ scripts/endpointconfig.js
@@ -0,0 +1,9 @@
+function getAEMPublish() {
+  return 'https://publish-p149891-e1546481.adobeaemcloud.com';
+}
+
+function getAEMAuthor() {
+  return 'https://author-p149891-e1546481.adobeaemcloud.com';
+}
+
+export { getAEMPublish, getAEMAuthor };
