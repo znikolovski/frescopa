457dc0194b195ffd404775d01066104e38c2ecfd
Zoran Nikolovski <nikolovs@adobe.com>
2025-03-12 18:28:02 +1100

    Changing over to my env

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p149891-e1546481.adobeaemcloud.com';
+  return 'https://publish-p115476-e1135027.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p149891-e1546481.adobeaemcloud.com';
+  return 'https://author-p115476-e1135027.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
