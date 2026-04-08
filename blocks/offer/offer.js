82be53037bd17741f71416679c3db21df5971144
Mark Szulc <mszulc@adobe.com>
2026-04-08 15:43:23 +1000

    fix: update URL condition to include 'ue.da.live' for author check

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -12,7 +12,7 @@ export default async function decorate(block) {
     variationname = variationElem.innerHTML.trim();
   }
 
-  const url = window.location && ((window.location.origin.includes('author') || window.location.origin.includes('experience.adobe.com')))
+  const url = window.location && ((window.location.origin.includes('author') || window.location.origin.includes('ue.da.live')))
     ? `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
     : `${aempublishurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
   const options = { credentials: 'include' };
