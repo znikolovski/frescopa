4857d1a6ce0495ee961ee8ffde9f80b789d28137
Mark Szulc <mszulc@adobe.com>
2026-04-08 15:30:43 +1000

    fix: correct URL construction logic for published offers

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -14,7 +14,7 @@ export default async function decorate(block) {
 
   const url = window.location && ((window.location.origin.includes('author') || window.location.href.includes('/aem/editor/canvas')))
     ? `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
-    : `${aempublishurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
+    : `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
   const options = { credentials: 'include' };
 
   const cfReq = await fetch(url, options)
