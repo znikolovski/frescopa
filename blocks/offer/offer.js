cedbe47bc087298223fc950260dcc5f45a36d75c
Mark Szulc <mszulc@adobe.com>
2026-04-08 15:06:44 +1000

    fix: correct URL construction logic for author and editor canvas paths

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -12,7 +12,7 @@ export default async function decorate(block) {
     variationname = variationElem.innerHTML.trim();
   }
 
-  const url = window.location && window.location.origin && window.location.origin.includes('author')
+  const url = window.location && (window.location.origin.includes('author') || window.location.pathname.includes('/aem/editor/canvas'))
     ? `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
     : `${aempublishurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
   const options = { credentials: 'include' };
